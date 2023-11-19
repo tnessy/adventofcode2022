1b17e9d523e0cd609238cb8b3619a8c76555aef7
tnessy <nsstrunks@gmail.com>
2023-11-18 22:53:21 -0500

    Day 2 part 2 impl... but I hate it

--- aoc2022-02/src/main.rs
+++ aoc2022-02/src/main.rs
@@ -1,9 +1,7 @@
-use std::collections::HashMap;
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("aoc2022-02/input.txt").expect("Expected file");
-    let _hand_score = HashMap::from([("X",1),("Y",2),("Z",3)]);
 
     let lines = contents.split("\n");
     let mut score = 0;
@@ -12,33 +10,27 @@ fn main() {
 
         if round[0] == "A" {
             match round[1] {
-                "X" => score+=3,
-                "Y" => score+=6,
-                "Z" => score+=0,
+                "X" => score+=3+0,
+                "Y" => score+=1+3,
+                "Z" => score+=2+6,
                 _ => score+=score
             }
         }
         if round[0] == "B" {
             match round[1] {
-                "X" => score+=0,
-                "Y" => score+=3,
-                "Z" => score+=6,
+                "X" => score+=1+0,
+                "Y" => score+=2+3,
+                "Z" => score+=3+6,
                 _ => score+=score
             }
         }
         if round[0] == "C" {
             match round[1] {
-                "X" => score+=6,
-                "Y" => score+=0,
-                "Z" => score+=3,
+                "X" => score+=2+0,
+                "Y" => score+=3+3,
+                "Z" => score+=1+6,
                 _ => score+=score
             }
-        }
-        match round[1] {
-            "X" => score+=1,
-            "Y" => score+=2,
-            "Z" => score+=3,
-            _ => score+=score
         }
     }
 
